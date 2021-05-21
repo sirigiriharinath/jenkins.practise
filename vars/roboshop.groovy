a3cb0cf3c9dcf57f7788d3b6fddb438a9b61a81d
Sasendra <Sasendra@sasendra.com>
2021-05-21 15:19:47 +0530

    hii

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -16,7 +16,7 @@ def call() {
     stage('Upload Artifacts') {
       steps {
         sh '''
-         curl -f -v -u admin:admin --upload-file frontend.zip http://54.204.17.34:8081/repository/Frontend/frontend.zip
+         curl -f -v -u admin:admin --upload-file frontend.zip http://10.1.2.210:8081/repository/Frontend/frontend.zip
         '''
       }
     }
