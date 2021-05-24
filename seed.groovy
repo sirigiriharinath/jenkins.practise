d11103850f4263f8066957fc03eed5a57582f3ca
Sasendra <Sasendra@sasendra.com>
2021-05-24 17:22:49 +0530

    login

--- seed.groovy
+++ seed.groovy
@@ -30,5 +30,26 @@ for (i in 0..count) {
   }
 }
 
+  pipelineJob("CI-Pipelines/login-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/sasender/login.git/'+j+'.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+}
 
 
