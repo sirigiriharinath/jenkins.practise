b6de97a0885cb26f5c3c92f5284846a4a2b7e36a
Sasendra <Sasendra@sasendra.com>
2021-05-24 17:44:58 +0530

    git removed

--- seed.groovy
+++ seed.groovy
@@ -36,7 +36,7 @@ for (i in 0..count) {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/sasender/login.git/'+j+'.git')
+              'url'('https://github.com/sasender/login.git')
             }
           }
           'branches' {
