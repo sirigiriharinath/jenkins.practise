14e4d5d4d2b1d098d1d6b099dbf978a6d6131bd5
Sasendra <Sasendra@sasendra.com>
2021-05-31 14:15:27 +0530

    un

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -11,12 +11,8 @@ def call(Map params = [:]) {
     agent {
       label "${args.SLAVE_LABEL}"
     }
-    triggers {
-       pollSCM('* * * * 1-5')
-    }
-    tools {
-        maven 'mvn'
-    }
+      
+  
 
   environment {
     COMPONENT       =  "${args.COMPONENT}"
